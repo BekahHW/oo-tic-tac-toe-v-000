dc0b8e05e1b278e0a32187d251a0a0e50968fc7f
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-06-30 17:13:47 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,43 @@
+class TicTacToe
+   def initialize(board = nil)
+   @board = board || Array.new(9, " ")
+ end
+ 
+   WIN_COMBINATIONS = [
+     [0,1,2],
+     [3,4,5],
+     [6,7,8],
+     [0,4,8],
+     [2,4,6],
+     [1,4,7],
+     [0,3,6],
+     [2,5,8]]
+     
+  def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+   puts "-----------"
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+   puts "-----------"
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
+ 
+ def input_to_index(user_input)
+   user_input.to_i - 1
+ end
+ 
+ def move(index, current_player = "X")
+    @board[index] = current_player
+ end
+ 
+ def position_taken?
+   @board[index] != " "
+ end
+ 
+ def valid_move?
+    !position_taken? && index.between?(0,8)
+  end
+  
+  def turn_count
+    
+ end 
+end
