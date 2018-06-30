c8d672d798b284fdf7ee4b7fd91bc7437ea5312d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-06-30 17:21:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,4 +40,30 @@ class TicTacToe
   def turn_count
     valid_move?.count
  end 
+ 
+ def current_player
+    turn_count.even? ? "X" : "O"
+  end
+
+  def turn
+    puts "Please enter a number (1-9):"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
+  end
+
+  def won?
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
+  end
+
 end
